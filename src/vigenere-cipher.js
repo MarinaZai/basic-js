070590d6ec424adb9ee44044a91b70c159375651
MarinaZai <Marintos34@gmail.com>
2022-10-12 11:28:41 +0300

    add vigenere solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,4 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {
+  NotImplementedError
+} = require('../extensions/index.js');
 
 /**
  * Implement class VigenereCipheringMachine that allows us to create
@@ -21,75 +23,68 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 class VigenereCipheringMachine {
   constructor(type = true) {
-    this.type = type;
     this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    this.type = type;
   }
   encrypt(message, key) {
     if (!message || !key || typeof message !== 'string' || typeof key !== 'string') throw new Error ("Incorrect arguments!");
-    let m = message.toUpperCase().split(' ').join(''), k = key.toUpperCase(), res = '', result = '';
-    for(;m.length > k.length;) k+=k
+    let newMessage = message.toUpperCase().split(' ').join('');
+    let newKey = key.toUpperCase();
+    let result = '';
+    let finalResult = '';
+    for(;newMessage.length > newKey.length;) 
+    newKey+=newKey
 
-    for (let i = 0; i < m.length; i++) {
-      let newAbc = [...this.abc].slice('')
-      let codeAbc = newAbc.concat(newAbc.splice(0, newAbc.indexOf(k[i]))).join('');
-      if (this.abc.includes(m[i])) res += codeAbc[this.abc.indexOf(m[i])]
-      else res += m[i]
+    for (let i = 0; i < newMessage.length; i++) {
+      let newAlphabet = [...this.alphabet].slice('')
+      let codeAlphabet = newAlphabet.concat(newAlphabet.splice(0, newAlphabet.indexOf(newKey[i]))).join('');
+
+      if (this.alphabet.includes(newMessage[i])) result += codeAlphabet[this.alphabet.indexOf(newMessage[i])]
+      else result += newMessage[i]
     }
     
-    let revM = res.split('').reverse();
+    let reverseMessage = result.split('').reverse();
 
     for (let i = 0; i < message.length; i++) {
-      if (message[i] != ' ') result += revM.pop()
-      else result += ' '
+      if (message[i] != ' ') finalResult += reverseMessage.pop()
+      else finalResult += ' '
     }
 
     if (this.type) {
-      console.log(result);
-      return result
+      return finalResult
     } else {
-      console.log(result.split('').reverse().join(''));
-      return result.split('').reverse().join('')
+      return finalResult.split('').reverse().join('')
     }
   }
 
-  decrypt(mes, key) {
-    if (!mes || !key || typeof mes !== 'string' || typeof key !== 'string') throw new Error ("Incorrect arguments!");
-    let m = mes.toUpperCase().split(' ').join(''), k = key.toUpperCase(), res = '', result = '';
-    for(;m.length > k.length;) k+=k
-
-    for (let i = 0; i < m.length; i++) {
-      let newAbc = [...this.abc].slice('')
-      let codeAbc = newAbc.concat(newAbc.splice(0, newAbc.indexOf(k[i]))).join('');
-
-      // console.log(this.abc.includes(m[i]));
-
-      if (this.abc.includes(m[i])) res += this.abc[codeAbc.indexOf(m[i])]
-      else res += m[i]
-    }
-    
-    let revM = res.split('').reverse();
-
-    for (let i = 0; i < mes.length; i++) {
-      if (mes[i] != ' ') result += revM.pop()
-      else result += ' '
-    }
-    
-    if (this.type) {
-      console.log(result);
-      return result
-    } else {
-      console.log(result.split('').reverse().join(''));
-      return result.split('').reverse().join('')
-    }
-  }
-/*   encrypt(message, key) {
-    if (!message || !key || typeof mes !== 'string' || typeof key !== 'string') throw new Error ("Incorrect arguments!");
-
-  }
   decrypt(encryptedMessage, key) {
-    if (!encryptedMessage || !key || typeof mes !== 'string' || typeof key !== 'string') throw new Error ("Incorrect arguments!");
+    if (!encryptedMessage || !key || typeof encryptedMessage !== 'string' || typeof key !== 'string') throw new Error ("Incorrect arguments!");
+    let newEncryptedMessage = encryptedMessage.toUpperCase().split(' ').join('')
+    let newKey = key.toUpperCase()
+    let res = ''
+    let result = '';
+    for(;newEncryptedMessage.length > newKey.length;) newKey+=newKey
 
-  } */
+    for (let i = 0; i < newEncryptedMessage.length; i++) {
+      let newAlphabet = [...this.alphabet].slice('')
+      let codeAlphabet = newAlphabet.concat(newAlphabet.splice(0, newAlphabet.indexOf(newKey[i]))).join('');
+      if (this.alphabet.includes(newEncryptedMessage[i])) res += this.alphabet[codeAlphabet.indexOf(newEncryptedMessage[i])]
+      else res += newEncryptedMessage[i]
+    }
+    
+    let reverseEncryptedMessage = res.split('').reverse();
+
+    for (let i = 0; i < encryptedMessage.length; i++) {
+      if (encryptedMessage[i] != ' ') result += reverseEncryptedMessage.pop()
+      else result += ' '
+    }
+    
+    if (this.type) {
+      return result
+    } else {
+      return result.split('').reverse().join('')
+    }
+  }
 }
 
 module.exports = {
